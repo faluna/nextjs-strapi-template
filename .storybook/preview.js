728a268dc77f63f50cf93f56f3ed29a93805219c
faluna <37044099+faluna@users.noreply.github.com>
2021-03-30 22:50:31 +0900

    update:sparkles: Setup storybook config for nextjs Link and Image

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
-import { addDecorator } from '@storybook/react';
+import { withNextRouter } from 'storybook-addon-next-router';
 import { ChakraProvider } from '@chakra-ui/react';
+import './__mock/NextImage';
 
-addDecorator((story) => <ChakraProvider>{story()}</ChakraProvider>);
+export const decorators = [
+  (Story) => (
+    <ChakraProvider>
+      <Story />
+    </ChakraProvider>
+  ),
+  withNextRouter({
+    path: '/',
+    asPath: '/',
+    query: {},
+    push() {},
+  }),
+];
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
-
