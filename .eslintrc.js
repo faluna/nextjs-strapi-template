e415f0863f04c47691a8fcc1b0da231b9e84eb46
faluna <37044099+faluna@users.noreply.github.com>
2021-05-03 00:02:22 +0900

    fix:bug: Fix eslint config for testing-library and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,55 +7,46 @@ module.exports = {
     commonjs: true,
   },
   // パーサー
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   // jsx を使います
   parserOptions: {
     ecmaVersion: 2018,
     ecmaFeatures: {
       jsx: true,
     },
-    sourceType: "module",
+    sourceType: 'module',
   },
   // React のバージョンは自動検出に
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-  plugins: [
-    "react-hooks",
-    "react",
-    "@typescript-eslint",
-    "aspida",
-    "testing-library",
-    "jest-dom",
-  ],
+  plugins: ['react-hooks', 'react', '@typescript-eslint', 'aspida', 'testing-library', 'jest-dom'],
   // 基本的にルールは recommended に従う
   // prettier 関連は配列の最後尾に書く
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:testing-library/recommended",
-    "plugin:jest-dom/recommended",
-    "plugin:aspida/recommended",
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
+    'plugin:aspida/recommended',
+    'prettier',
   ],
   rules: {
     // TypeScirpt なので prop-types は要らない
-    "react/prop-types": "off",
+    'react/prop-types': 'off',
   },
   // .js ファイルをオーバーライド （webpack.conig.jsなど）
   overrides: [
     {
-      files: ["*.js"],
+      files: ['*.js'],
       rules: {
-        "@typescript-eslint/no-var-requires": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
       },
     },
   ],
