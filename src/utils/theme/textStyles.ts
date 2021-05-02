f81c2d79028c7217b715ff18f4cfc173c62c7d17
faluna <37044099+faluna@users.noreply.github.com>
2021-05-02 16:40:38 +0900

    fix:+1: Change Text style

--- src/utils/theme/textStyles.ts
+++ src/utils/theme/textStyles.ts
@@ -1,60 +1,60 @@
 const textStyles = {
   h1: {
-    fontSize: ['2.8125rem'],
+    fontSize: ['4rem'],
     fontWeight: 'bold',
-    lineHeight: '4rem',
+    lineHeight: [1.5],
   },
   h2: {
-    fontSize: ['2rem'],
+    fontSize: ['2.8125rem'],
     fontWeight: 'bold',
-    lineHeight: '2.5rem',
+    lineHeight: [1.6],
   },
   h3: {
-    fontSize: ['1.4375rem'],
+    fontSize: ['2rem'],
     fontWeight: 'semibold',
-    lineHeight: '2rem',
+    lineHeight: [1.75],
   },
   h4: {
-    fontSize: ['1rem'],
+    fontSize: ['1.4375rem'],
     fontWeight: 'semibold',
-    lineHeight: '1.5rem',
+    lineHeight: [1.75],
   },
   h5: {
     fontSize: ['1rem'],
     fontWeight: 'semibold',
-    lineHeight: '1.5rem',
+    lineHeight: [2],
   },
   xs: {
     fontSize: ['0.5rem'],
-    lineHeight: '1rem',
+    lineHeight: [2],
   },
   sm: {
     fontSize: ['0.75rem'],
-    lineHeight: '1.5rem',
+    lineHeight: [2],
   },
   md: {
     fontSize: ['1rem'],
-    lineHeight: '1.5rem',
+    lineHeight: [2],
   },
   lg: {
     fontSize: ['1.4375rem'],
-    lineHeight: '2rem',
+    lineHeight: [1.75],
   },
   xl: {
     fontSize: ['2rem'],
-    lineHeight: '2.5rem',
+    lineHeight: [1.75],
   },
   '2xl': {
     fontSize: ['2.8125rem'],
-    lineHeight: '4rem',
+    lineHeight: [1.6],
   },
   '3xl': {
     fontSize: ['4rem'],
-    lineHeight: '5rem',
+    lineHeight: [1.5],
   },
   '4xl': {
     fontSize: ['5.6875rem'],
-    lineHeight: '6.5rem',
+    lineHeight: [1.4],
   },
 };
 
