195a2118c322f3da4750a694c0405a15973f6d8b
faluna <37044099+faluna@users.noreply.github.com>
2021-05-02 19:16:10 +0900

    fix:bug: Fix storybook main.js due to path alias

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async (config) => {
     return {
       ...config,
@@ -12,8 +12,18 @@ module.exports = {
         ...config.resolve,
         alias: {
           ...config.resolve.alias,
-          "@emotion/core": toPath("node_modules/@emotion/react"),
-          "emotion-theming": toPath("node_modules/@emotion/react"),
+          atoms: toPath('src/atoms'),
+          molecules: toPath('src/molecules'),
+          organisms: toPath('src/organisms'),
+          templates: toPath('src/templates'),
+          pages: toPath('src/pages'),
+          utils: toPath('src/utils'),
+          tests: toPath('src/__tests__'),
+          api: toPath('src/api'),
+          types: toPath('src/types'),
+          '~': toPath('src'),
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
         },
       },
     };
