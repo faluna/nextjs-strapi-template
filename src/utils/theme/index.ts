ad30bc853a9a215bd932325093eb684b652868ce
faluna <37044099+faluna@users.noreply.github.com>
2021-05-09 12:22:21 +0900

    add:tada: Make style.ts for global styles

--- src/utils/theme/index.ts
+++ src/utils/theme/index.ts
@@ -2,8 +2,10 @@ import { extendTheme } from '@chakra-ui/react';
 
 import textStyles from './textStyles';
 import foundations from './foundations';
+import styles from './styles';
 
 const overrides = {
+  styles,
   textStyles,
   ...foundations,
 };
---
+++ src/utils/theme/styles.ts
@@ -0,0 +1,9 @@
+const styles = {
+  global: (props) => ({
+    body: {
+      letterSpacing: '0.05em',
+    },
+  }),
+};
+
+export default styles;
